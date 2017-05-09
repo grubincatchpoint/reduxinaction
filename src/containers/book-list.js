b71e6e342fff32e675f20df7fcec51f32d3c8cce
Greg Rubin <grubin@catchpoint.com>
2017-05-09 17:30:11 -0400

    Implementation of a container class

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-export default class BookList extends Component {
+import {connect} from 'react-redux';
+
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
@@ -15,3 +17,13 @@ export default class BookList extends Component {
     );
   };
 }
+
+function mapStateToProps(state) {
+  //Whatever is returned will show up as props
+  //inside of BookList
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
